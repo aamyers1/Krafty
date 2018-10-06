676e3247d720a6fa2a029c4305175d91c35a2cd1
Alissa Myers <aamyers1@svsu.edu>
2018-10-06 15:01:55 -0400

    initial commit

---
+++ app/src/main/java/com/team6/krafty/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.team6.krafty;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
