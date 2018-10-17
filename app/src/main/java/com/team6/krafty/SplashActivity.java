ca8b225063053c23e8db2431570262ebbfc01494
Alissa Myers <aamyers1@svsu.edu>
2018-10-17 16:58:34 -0400

    Added 'splash page' achievable upon login

---
+++ app/src/main/java/com/team6/krafty/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.team6.krafty;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+}
