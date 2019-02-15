e09b363d961855508c9df014acdaac07e84fc162
Masterpickl1 <dbruderick@hotmail.com>
2019-02-14 22:38:42 -0500

    Event image select, krafter number, time pick added. Switches sized and added labels for them.

--- app/src/main/java/com/team6/krafty/CreateEventActivity.java
+++ app/src/main/java/com/team6/krafty/CreateEventActivity.java
@@ -4,21 +4,27 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.location.Address;
 import android.location.Geocoder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.time.Clock;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -36,6 +42,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 public class CreateEventActivity  extends AppCompatActivity {
+    private String encodedImage = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +50,14 @@ public class CreateEventActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_create_event);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//        ImageView iv = findViewById(R.id.matImage);
-//        iv.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //start activity passing pick media intent and PICK_IMAGE code (100)
-//                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI), 100);
-//            }
-//        });
+        ImageView iv = findViewById(R.id.imgEvent);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start activity passing pick media intent and PICK_IMAGE code (100)
+                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI), 100);
+            }
+        });
 
 
 
@@ -62,8 +69,8 @@ public class CreateEventActivity  extends AppCompatActivity {
                 int mYear = c.get(Calendar.YEAR);
                 int mMonth = c.get(Calendar.MONTH);
                 int mDay = c.get(Calendar.DAY_OF_MONTH);
-                DatePickerDialog dateDialog= new DatePickerDialog(v.getContext(), startDatePickerListener, mYear, mMonth, mDay);
-                dateDialog.show();
+                DatePickerDialog dateStartDialog= new DatePickerDialog(v.getContext(), startDatePickerListener, mYear, mMonth, mDay);
+                dateStartDialog.show();
 
             }
         });
@@ -76,8 +83,36 @@ public class CreateEventActivity  extends AppCompatActivity {
                 int mYear = c.get(Calendar.YEAR);
                 int mMonth = c.get(Calendar.MONTH);
                 int mDay = c.get(Calendar.DAY_OF_MONTH);
-                DatePickerDialog dateDialog= new DatePickerDialog(v.getContext(), endDatePickerListener, mYear, mMonth, mDay);
-                dateDialog.show();
+                DatePickerDialog dateEndDialog= new DatePickerDialog(v.getContext(), endDatePickerListener, mYear, mMonth, mDay);
+                dateEndDialog.show();
+
+            }
+        });
+
+        Button btnGetClose = findViewById(R.id.btnGetEndTime);
+        btnGetClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int mHour = c.get(Calendar.HOUR_OF_DAY);
+                int mMinute = c.get(Calendar.MINUTE);
+                boolean is24Hour = false;
+                TimePickerDialog timeEndDialog= new TimePickerDialog(v.getContext(), endTimePickerListener, mHour, mMinute, is24Hour);
+                timeEndDialog.show();
+
+            }
+        });
+
+        Button btnGetOpen = findViewById(R.id.btnGetStartTime);
+        btnGetOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int mHour = c.get(Calendar.HOUR_OF_DAY);
+                int mMinute = c.get(Calendar.MINUTE);
+                boolean is24Hour = false;
+                TimePickerDialog timeStartDialog= new TimePickerDialog(v.getContext(), startTimePickerListener, mHour, mMinute, is24Hour);
+                timeStartDialog.show();
 
             }
         });
@@ -94,16 +129,6 @@ public class CreateEventActivity  extends AppCompatActivity {
 
 
 
-    public static java.util.Date getDateFromDatePicker(DatePicker datePicker){
-        int day = datePicker.getDayOfMonth();
-        int month = datePicker.getMonth();
-        int year =  datePicker.getYear();
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, day);
-
-        return calendar.getTime();
-    }
     //handles submit click
     public void onSubmitClick(){
 //        //essentially just gathers strings from various editTexts
@@ -151,18 +176,63 @@ public class CreateEventActivity  extends AppCompatActivity {
     private DatePickerDialog.OnDateSetListener startDatePickerListener = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            String dateYouChoosed = (monthOfYear + 1) + "/" + dayOfMonth  + "/" + year;
+            String dateYouChose = (monthOfYear + 1) + "/" + dayOfMonth  + "/" + year;
             TextView tvStartDate = findViewById(R.id.tvStartDate);
-            tvStartDate.setText(dateYouChoosed );
+            tvStartDate.setText(dateYouChose );
         }
     };
 
     private DatePickerDialog.OnDateSetListener endDatePickerListener = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            String dateYouChoosed =  (monthOfYear + 1) + "/" + dayOfMonth + "/" + year;
+            String dateYouChose =  (monthOfYear + 1) + "/" + dayOfMonth + "/" + year;
             TextView tvEndDate = findViewById(R.id.tvEndDate);
+            tvEndDate.setText(dateYouChose );
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener endTimePickerListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hour, int minute) {
+            String dateYouChoosed =  (hour + 1) + ":" + minute;
+            TextView tvEndDate = findViewById(R.id.tvEndTime);
             tvEndDate.setText(dateYouChoosed );
         }
     };
+
+    private TimePickerDialog.OnTimeSetListener startTimePickerListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hour, int minute) {
+            String dateYouChoosed =  (hour + 1) + ":" + minute;
+            TextView tvEndDate = findViewById(R.id.tvStartTime);
+            tvEndDate.setText(dateYouChoosed );
+        }
+    };
+
+    //For results of requests
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //If result was ok and was of PICK_IMAGE activity
+        if(resultCode == RESULT_OK && requestCode == 100){
+            Uri imageUri = data.getData();
+
+            //Put image in imageview
+            ImageView imgProfile = findViewById(R.id.imgEvent);
+            imgProfile.setImageURI(imageUri);
+
+            //Convert image to bitmap, then into base64
+            Bitmap imageAsBitmap = null;
+            try {
+                imageAsBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+            } catch (IOException e) { //for file not found
+                e.printStackTrace();
+            }
+            ByteArrayOutputStream byteArrOutStrm = new ByteArrayOutputStream();
+            imageAsBitmap.compress(Bitmap.CompressFormat.JPEG,100, byteArrOutStrm);
+            byte[] bArr = byteArrOutStrm.toByteArray();
+            encodedImage = Base64.encodeToString(bArr, Base64.DEFAULT).replace("+", "<");
+        }
+    }
 }
