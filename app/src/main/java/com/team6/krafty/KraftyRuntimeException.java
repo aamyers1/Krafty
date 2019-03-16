0200bcc60ce4668c6a25ecee5889a15e3ba943c3
masterpickl1 <masterpickl1@gmail.com>
2019-03-16 14:42:06 -0400

    Custom Krafty exceptions (Checked, Unchecked): KraftyException and KraftyRuntimeException

---
+++ app/src/main/java/com/team6/krafty/KraftyRuntimeException.java
@@ -0,0 +1,18 @@
+package com.team6.krafty;
+
+//Runtime or Unchecked exception not checked at compiled time
+public class KraftyRuntimeException extends RuntimeException {
+    public KraftyRuntimeException(String errorMessage, Throwable err){
+        super(errorMessage,err);
+    }
+}
+
+/* To use:
+    try {
+        *do data things*
+    } catch (IllegalArgumentException e) {
+        if (! isNumeric(data)) {
+            throw new KraftyRunTimeException("Input must be numeric : " + data, e);
+        }
+    }
+*/
